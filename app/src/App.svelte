d2ef47707d0ec3bba5b63d0d9657b647c7b22e63
SG <who.just.the.doctor@gmail.com>
2022-10-25 09:38:21 +1000

    app folder

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,6 +1,26 @@
 <script>
     import { Line } from "svelte-chartjs";
-    import "chart.js/auto";
+    import {
+        Chart as ChartJS,
+        Title,
+        Tooltip,
+        Legend,
+        LineElement,
+        LinearScale,
+        PointElement,
+        CategoryScale,
+    } from "chart.js";
+
+    ChartJS.register(
+        Title,
+        Tooltip,
+        Legend,
+        LineElement,
+        LinearScale,
+        PointElement,
+        CategoryScale
+    );
+
     import api_get from "./lib/api.js";
     import BranchSelector from "./lib/BranchSelector.svelte";
     import Files from "./lib/Files.svelte";
